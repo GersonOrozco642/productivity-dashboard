4fdd5b8244aed4975ed73a8c55b7074341bb10a9
Gerzon Orozco <ogerson642@gmail.com>
2026-05-30 04:24:12 -0700

    feat: add JPA repositories for Task, Streak, and Transaction entities

--- src/main/java/com/gerzon/productivity_dashboard/repository/StreakRepository.java
+++ src/main/java/com/gerzon/productivity_dashboard/repository/StreakRepository.java
@@ -1,9 +1,9 @@
 package com.gerzon.productivity_dashboard.repository;
 
-import com.gerzon.productivity_dashboard.model.Task;
+import com.gerzon.productivity_dashboard.model.Streak;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface StreakRepository extends JpaRepository<Streak, Long> {
 }
--- src/main/java/com/gerzon/productivity_dashboard/repository/TransactionRepository.java
+++ src/main/java/com/gerzon/productivity_dashboard/repository/TransactionRepository.java
@@ -1,9 +1,9 @@
 package com.gerzon.productivity_dashboard.repository;
 
-import com.gerzon.productivity_dashboard.model.Task;
+import com.gerzon.productivity_dashboard.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
